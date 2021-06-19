f6b7d2f5c171c335efc98a2932c9ea221d726d06
KindaExists <kindaexists@gmail.com>
2021-06-19 18:48:10 +0800

    created working version

--- webcam_test.py
+++ webcam_test.py
@@ -3,18 +3,27 @@ import cv2
 cv2.namedWindow("test")
 vc = cv2.VideoCapture(0)
 
+gscale = " .:-=+*#%@"
+# 10 Levels of grayscale
+
 if vc.isOpened():
     rval, frame = vc.read()
 else:
     rval = False
 
+w, h = 160, 120
+
 while rval:
     rval, frame = vc.read()
-    frame = cv2.resize(frame, (160, 120))
+    frame = cv2.resize(frame, (w, h))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("preview", gray)
 
-    if cv2.waitKey(20) == 27:
+    cv2.imshow("test", gray)
+
+    thing = [[gscale[round(gray[y, x] * (9/256))] for x in range(w)] for y in range(h)]
+    print('\n'.join(['  '.join(y) for y in thing]))
+
+    if cv2.waitKey(20) == 27: # ESC key
         break
 
 vc.release()
